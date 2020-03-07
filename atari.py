166a2089f0ee8490b0aec3e1ee12a9c384dfe40a
Benjamin WALLYN <benjamin.wallyn@ensta-paristech.fr>
2020-03-07 21:25:39 +0100

    Update atari.py

--- atari.py
+++ atari.py
@@ -166,7 +166,7 @@ class Atari:
 
                 if terminal:
                     l_scores.append(score)
-                    print("Score: ", str(score))
+                    print(score)
                     game_model.save_run(score, step, run)
                     break
 
